5f32d0f1abd72811636cdfc1502d4489c5bd7e36
Kevin Eliezer <kevinel.gp@gmail.com>
2020-04-14 11:15:06 +0100

    Lo que se ha hecho hasta ahora

---
+++ Practica7Firebase/index.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBd2QXd4Xw3ssVhgQ9SBDrD1krb27s8DyA",
+    authDomain: "project-id.firebaseapp.com",
+    databaseURL: "https://project-id.firebaseio.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "sender-id",
+    appID: "banco-del-tiempo",
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
